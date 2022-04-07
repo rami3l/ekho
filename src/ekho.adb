c59f155f67e9e0d289dc35b235de704117db22a3
rami3l <rami3l@outlook.com>
2022-04-07 16:05:57 +0200

    feat: make the echo server actually echo

--- src/ekho.adb
+++ src/ekho.adb
@@ -20,19 +20,28 @@ procedure Ekho is
 
    task body Ping is
       Channel : Socket_Type;
+
+      Listener    : Libekho.Listener.Listener;
+      Peer_Socket : Socket_Type;
+      Peer_Addr   : Sock_Addr_Type;
    begin
       accept Start;
-      Put_Line ("Sender launched!");
-      Put_Line ("Sender: Creating socket...");
+      Put_Line ("Ping launched!");
+      Put_Line ("Ping: Creating socket...");
       Create_Socket (Channel);
-      Put_Line ("Sender: Connecting socket " & Channel'Image);
+      Put_Line ("Ping: Connecting socket " & Channel'Image);
       Connect_Socket (Socket => Channel, Server => Addr);
-      Put_Line ("Sender: Sending message...");
+      Put_Line ("Ping: Sending message...");
       Unbounded_String'Write
         (Stream (Channel), To_Unbounded_String ("HeLlo the S0ck3t w0r1d!"));
-      Put_Line ("Sender: Closing socket...");
-      Close_Socket (Channel);
-      accept Stop;
+      declare
+         Received : String := String'Input (Stream (Channel));
+      begin
+         Put_Line ("Ping Received: " & Received);
+         Put_Line ("Ping: Closing socket...");
+         Close_Socket (Channel);
+         accept Stop;
+      end;
    end Ping;
 
    task body Pong is
@@ -42,14 +51,16 @@ procedure Ekho is
    begin
       Libekho.Listener.Bind (Addr, Listener);
       accept Start;
-      Put_Line ("Listener: Accepting peer socket...");
+      Put_Line ("Pong: Accepting peer socket...");
       Listener.Accept_Incoming (Peer_Socket, Peer_Addr);
-      Put_Line ("Listener: Peer socket accepted.");
+      Put_Line ("Pong: Peer socket accepted.");
       declare
          Received : String := String'Input (Stream (Peer_Socket));
       begin
-         Put_Line ("Listener Received: " & Received);
-         Put_Line ("Listener: Closing peer socket...");
+         Put_Line ("Pong Received: " & Received);
+         Unbounded_String'Write
+           (Stream (Peer_Socket), To_Unbounded_String (Received));
+         Put_Line ("Pong: Closing peer socket...");
          Close_Socket (Peer_Socket);
       end;
       accept Stop;
