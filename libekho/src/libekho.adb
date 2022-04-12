b68e841c47b4f69e7e15fcc98d94a318f4e97b7a
rami3l <rami3l@outlook.com>
2022-04-12 16:04:29 +0200

    feat: (wip) integrate `RecordFlux`-generated code (`Read` not working)

--- libekho/src/libekho.adb
+++ libekho/src/libekho.adb
@@ -1,7 +1,12 @@
+with Ada.Streams; use Ada.Streams;
 with Ada.Unchecked_Deallocation;
 with RFLX.Ekho.Packet;
 with RFLX.Ekho; use RFLX.Ekho;
+with RFLX.RFLX_Builtin_Types;
 with RFLX.RFLX_Types;
+with Libekho.Compat; use Libekho.Compat;
+
+with Ada.Text_IO;
 
 package body Libekho is
    package Types renames RFLX.RFLX_Types;
@@ -15,7 +20,7 @@ package body Libekho is
       Item   : in Message)
    is
       Buffer : Types.Bytes_Ptr :=
-        new Types.Bytes (Types.Index'First .. Types.Index (255));
+        new Types.Bytes (1 .. Types.Index (1 + 255));
       Context : Packet.Context;
       procedure Free is new Ada.Unchecked_Deallocation
         (Types.Bytes, Types.Bytes_Ptr);
@@ -37,16 +42,37 @@ package body Libekho is
      (Stream : not null access Ada.Streams.Root_Stream_Type'Class)
       return Message
    is
-      Size : Str_Size;
+      Size : Stream_Element_Array (1 .. 1);
+      Size_Offset: Stream_Element_Offset;
+      Buffer : Stream_Element_Array (1 .. (1+255));
+      Last : Stream_Element_Offset;
+      Context : Packet.Context;
+
+      procedure Free is new Ada.Unchecked_Deallocation
+        (Types.Bytes, Types.Bytes_Ptr);
    begin
-      Str_Size'Read (Stream, Size);
+      Read (Stream.all, Size, Last);
+      Buffer(1) := Size(1);
+      Size_Offset := Stream_Element_Offset(Size(1));
+      Ada.Text_IO.Put_Line("SIZE: " & Size'Image);
+      Read (Stream.all, Buffer(2..(1+Size_Offset)), Last);
+      Ada.Text_IO.Put_Line("BUFFER: " & Buffer(1..(1+ Size_Offset))'Image);
       declare
-         Str : String (1 .. Integer (Size));
+         Bytes_Buffer : Types.Bytes_Ptr := new Types.Bytes'(To_RFLX_Bytes(Buffer(1..(1+Size_Offset))));
       begin
-         String'Read (Stream, Str);
-         return (Size => Message_Size_Type (Size), Str => Str);
-         -- NOTE: Returning to the Secondary Stack.
-         -- https://docs.adacore.com/gnat_ugx-docs/html/gnat_ugx/gnat_ugx/the_stacks.html
-      end;
+         Ada.Text_IO.Put_Line("BYTES_BUFFER: " & Bytes_Buffer.all'Image);
+         Packet.Initialize (Context, Bytes_Buffer);
+         Packet.Verify_Message (Context);
+         declare
+            Size : constant Message_Size_Type := Message_Size_Type(Size_Offset);
+            Str_Bytes : Types.Bytes (1.. Types.Index(Size));
+         begin
+            Packet.Get_Str (Context, Str_Bytes);
+            Free (Bytes_Buffer);
+            return (Size => Size, Str => [for C of Str_Bytes => Character'Val (C)]);
+            -- NOTE: Returning to the Secondary Stack.
+            -- https://docs.adacore.com/gnat_ugx-docs/html/gnat_ugx/gnat_ugx/the_stacks.html
+         end;
+     end;
    end Read;
 end Libekho;
